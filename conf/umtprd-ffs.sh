dbccb1aaa8c4b92f0c807c0b48c367ce08d51632
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2019-07-17 20:38:37 +0200

    FunctionFS entry changed to ffs.mtp

--- conf/umtprd-ffs.sh
+++ conf/umtprd-ffs.sh
@@ -13,7 +13,7 @@ cd cfg/usb_gadget/g1
 
 mkdir configs/c.1
 
-mkdir functions/ffs.umtp
+mkdir functions/ffs.mtp
 # Uncomment / Change the follow line to enable another usb gadget function
 #mkdir functions/acm.usb0
 
@@ -30,12 +30,12 @@ echo "The Viveris Product !" > strings/0x409/product
 echo "Conf 1" > configs/c.1/strings/0x409/configuration
 echo 120 > configs/c.1/MaxPower
 
-ln -s functions/ffs.umtp configs/c.1
+ln -s functions/ffs.mtp configs/c.1
 # Uncomment / Change the follow line to enable another usb gadget function
 #ln -s functions/acm.usb0 configs/c.1
 
-mkdir /dev/ffs-umtp
-mount -t functionfs mtp /dev/ffs-umtp
+mkdir /dev/ffs-mtp
+mount -t functionfs mtp /dev/ffs-mtp
 # Start the umtprd service
 umtprd &
 
