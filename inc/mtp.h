0f081de6500a5f22d81f229e92a64b676c97f6c7
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2019-09-02 19:57:41 +0200

    version v0.11.1

--- inc/mtp.h
+++ inc/mtp.h
@@ -130,6 +130,6 @@ int mtp_push_event(mtp_ctx * ctx, uint32_t event, int nbparams, uint32_t * param
 
 void mtp_deinit_responder(mtp_ctx * ctx);
 
-#define APP_VERSION "v0.10.1"
+#define APP_VERSION "v0.11.1"
 
 #endif
