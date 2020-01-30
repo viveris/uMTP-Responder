961e25e143a04c9d2c61d5307c1bd3f39f0d1296
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2020-01-30 15:46:21 +0100

    v1.3.5

--- inc/mtp.h
+++ inc/mtp.h
@@ -153,6 +153,6 @@ int build_response(mtp_ctx * ctx, uint32_t tx_id, uint16_t type, uint16_t status
 int check_and_send_USB_ZLP(mtp_ctx * ctx , int size);
 int parse_incomming_dataset(mtp_ctx * ctx,void * datain,int size,uint32_t * newhandle, uint32_t parent_handle, uint32_t storage_id);
 
-#define APP_VERSION "v1.3.0"
+#define APP_VERSION "v1.3.5"
 
 #endif
