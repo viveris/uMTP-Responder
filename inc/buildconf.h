90d655e7a99e022a251f3d76e46a3cabe1941422
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2020-10-13 09:52:59 +0200

    Reduce the default max usb read/write buffer size to 8KB. Some targets don't support 64KB buffer size (iMX family...). These parameters can be changed in the config file.

--- inc/buildconf.h
+++ inc/buildconf.h
@@ -3,8 +3,8 @@
 
 //#define CONFIG_USB_NON_BLOCKING_WRITE 1
 
-#define CONFIG_READ_FILE_BUFFER_SIZE (1024*1024)  // Must be a 2^x value.
-#define CONFIG_MAX_TX_USB_BUFFER_SIZE (128*512)   // Must be a multiple of 512 and be less than CONFIG_READ_FILE_BUFFER_SIZE 
-#define CONFIG_MAX_RX_USB_BUFFER_SIZE (128*512)   // Must be a multiple of 512
+#define CONFIG_READ_FILE_BUFFER_SIZE  (1024*1024) // Must be a 2^x value.
+#define CONFIG_MAX_TX_USB_BUFFER_SIZE (16*512)    // Must be a multiple of 512 and be less than CONFIG_READ_FILE_BUFFER_SIZE 
+#define CONFIG_MAX_RX_USB_BUFFER_SIZE (16*512)    // Must be a multiple of 512
 
 #include "custom_buildconf.h"
