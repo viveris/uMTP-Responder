d3d4823bb0e81aff5b14db3fd2870dbddfee549b
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2024-07-08 20:45:37 +0200

    (c) dates updated.

--- inc/mtp_operations.h
+++ inc/mtp_operations.h
@@ -1,6 +1,6 @@
 /*
  * uMTP Responder
- * Copyright (c) 2018 - 2021 Viveris Technologies
+ * Copyright (c) 2018 - 2024 Viveris Technologies
  *
  * uMTP Responder is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
--- src/umtprd.c
+++ src/umtprd.c
@@ -1,6 +1,6 @@
 /*
  * uMTP Responder
- * Copyright (c) 2018 - 2021 Viveris Technologies
+ * Copyright (c) 2018 - 2024 Viveris Technologies
  *
  * uMTP Responder is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
@@ -141,7 +141,7 @@ int main(int argc, char *argv[])
 	PRINT_MSG("Version: %s compiled the %s@%s", APP_VERSION,
 		  __DATE__, __TIME__);
 
-	PRINT_MSG("(c) 2018 - 2021 Viveris Technologies");
+	PRINT_MSG("(c) 2018 - 2024 Viveris Technologies");
 
 	if(argc>1)
 	{
